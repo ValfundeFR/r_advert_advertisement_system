ec161d3ef6fd1cdd2f4caacd1bf75fdde5d30915
Raphaël <valfunde@gmx.fr>
2017-04-01 12:35:03 +0200

    Update cl_init.lua

--- r_advert_advertisement_system_1.1/lua/entities/advert_npc/cl_init.lua
+++ r_advert_advertisement_system_1.1/lua/entities/advert_npc/cl_init.lua
@@ -1,6 +1,6 @@
 --[[-------------------------------------------------------------------------
 Script by Valfunde
-If you have any problems, don't hesitate to leave a ScriptFodder ticket and/or add me on steam (Valfunde *Banna Army)
+If you have any problems, don't hesitate to make an issues on GitHub and/or add me on steam (Valfunde *Banna Army)
 If you try to edit something wich isn't in configuration file and the script doesn't work, it's you fault so don't come cry and tell script is broken.
 ---------------------------------------------------------------------------]]
 
